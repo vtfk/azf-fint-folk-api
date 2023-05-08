4e5efdd44f87d9ab393f6f335f8b39bb59b2d8c9
jorgtho <jorgen.thorsnes@gmail.com>
2023-05-08 14:00:51 +0200

    added organization endpoint

---
+++ Organization/index.js
@@ -0,0 +1,54 @@
+const fintOrganization = require('../lib/fint-organization')
+const { logger, logConfig } = require('@vtfk/logger')
+const { decodeAccessToken } = require('../lib/helpers/decode-access-token')
+const httpResponse = require('../lib/requests/http-response')
+const { roles } = require('../config')
+
+module.exports = async function (context, req) {
+  logConfig({
+    prefix: 'azf-fint-folk - Organization',
+    azure: {
+      context,
+      excludeInvocationId: false
+    }
+  })
+  logger('info', ['New Request. Validating token'])
+  const decoded = decodeAccessToken(req.headers.authorization)
+  if (!decoded.verified) {
+    logger('warn', ['Token is not valid', decoded.msg])
+    return httpResponse(401, decoded.msg)
+  }
+  logConfig({
+    prefix: `azf-fint-folk - Organization - ${decoded.appid}${decoded.upn ? ' - ' + decoded.upn : ''}`,
+    azure: {
+      context,
+      excludeInvocationId: false
+    }
+  })
+  logger('info', ['Token is valid, checking params'])
+  if (!req.params) {
+    logger('info', ['No params here...'])
+    return httpResponse(400, 'Missing query params')
+  }
+
+  const { identifikator, identifikatorverdi } = req.params
+  const validIdentifiers = ['organisasjonsId', 'organisasjonsKode']
+  if (!validIdentifiers.includes(identifikator)) return httpResponse(400, `Query param ${identifikator} is not valid - must be ${validIdentifiers.join(' or ')}`)
+
+  logger('info', ['Validating role'])
+  if (!decoded.roles.includes(roles.organizationRead)) {
+    logger('info', ['Missing required role for access'])
+    return httpResponse(403, 'Missing required role for access')
+  }
+  logger('info', ['Role validated'])
+
+  try {
+    const res = await fintOrganization(identifikator, identifikatorverdi)
+    if (!res) return httpResponse(404, `No organizationUnit with ${identifikator} "${identifikatorverdi}" found in FINT`)
+    const result = req.query.includeRaw === 'true' ? { ...res.repacked, raw: res.raw } : res.repacked
+    return httpResponse(200, result)
+  } catch (error) {
+    logger('error', [error])
+    return { status: 500, body: error.toString() }
+  }
+}
---
+++ fint-templates/organization.js
@@ -0,0 +1,114 @@
+module.exports = (identifikator, identifikatorverdi) => {
+  return {
+    query: `
+      query {
+        organisasjonselement(${identifikator}: "${identifikatorverdi}") {
+					organisasjonsId {
+						identifikatorverdi
+					}
+					organisasjonsKode {
+						identifikatorverdi
+					}
+					gyldighetsperiode {
+						start
+						slutt
+					}
+					navn
+					kortnavn
+					kontaktinformasjon {
+						epostadresse
+						mobiltelefonnummer
+						telefonnummer
+						nettsted
+					}
+					postadresse {
+						adresselinje
+						postnummer
+						poststed
+					}
+					organisasjonsnummer {
+						identifikatorverdi
+					}
+					forretningsadresse {
+						adresselinje
+						postnummer
+						poststed
+					}
+					organisasjonsnavn
+					leder {
+						ansattnummer {
+							identifikatorverdi
+						}
+						person {
+							navn {
+								fornavn
+								mellomnavn
+								etternavn
+							}
+						}
+					}
+					ansvar {
+						kode
+						navn
+					}
+					overordnet {
+						organisasjonsId {
+							identifikatorverdi
+						}
+						gyldighetsperiode {
+							start
+							slutt
+						}
+						navn
+						kortnavn
+					}
+					underordnet {
+						organisasjonsId {
+							identifikatorverdi
+						}
+						gyldighetsperiode {
+							start
+							slutt
+						}
+						navn
+						kortnavn
+					}
+					arbeidsforhold {
+						systemId {
+							identifikatorverdi
+						}
+						arbeidsforholdsperiode {
+							start
+							slutt
+						}
+						arbeidsforholdstype {
+							kode
+							navn
+						}
+						gyldighetsperiode {
+							start
+							slutt
+						}
+						stillingstittel
+						personalressurs {
+							ansattnummer {
+								identifikatorverdi
+							}
+							ansettelsesperiode {
+								start
+								slutt
+							}
+							person {
+								navn {
+									fornavn
+									mellomnavn
+									etternavn
+								}
+							}
+						}
+					}
+				}
+      }
+    `
+  }
+}
