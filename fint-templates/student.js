fa026cb3412157333fe09c47df53674639f40564
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2024-09-11 13:47:25 +0200

    added grepreferanse to students programomrade

--- fint-templates/student.js
+++ fint-templates/student.js
@@ -81,6 +81,7 @@ module.exports = (feidenavn) => {
                 systemId {
                   identifikatorverdi
                 }
+                grepreferanse
                 utdanningsprogram {
                   systemId {
                     identifikatorverdi
